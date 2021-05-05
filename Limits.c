743a194273066bd83f5c4fccba885e11ebcffcd1
Yuseung Lee <phillip9807@gmail.com>
2021-05-05 16:05:08 +0900

    add Limits

---
+++ Limits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	printf("Long_MIN: %ld \n", LONG_MIN);
+	printf("Long_MAX: %ld \n", LONG_MAX);
+	
+	printf("Char_MIN: %d \n", CHAR_MIN);
+	printf("Char_MAX: %d \n", CHAR_MAX);
+	
+	printf("Char_BIT: %d \n", CHAR_BIT);
+}
